1ed32bd8d7feb14a7d57496e60c02642e6a9614b
harviando <harviando1@gmail.com>
2023-11-30 11:04:28 +0800

    add code to move the mouse in intervals

--- script.py
+++ script.py
@@ -4,7 +4,10 @@ import time
 import keyboard
 import threading
 
+event = threading.Event()
 while True:
     x = random.randint(600, 700)
+    y = random.randint(200, 600)
     print(x)
     time.sleep(0.5)
+    pag.moveTo(x, y, 0.5)
