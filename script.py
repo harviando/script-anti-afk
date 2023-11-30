04e007980c1487c2b47dfef8635dcbd84841831f
harviando <harviando1@gmail.com>
2023-11-30 11:29:57 +0800

    adjust and add the listener of keypress 'e' to end the program

--- script.py
+++ script.py
@@ -2,12 +2,13 @@ import pyautogui as pag
 import random
 import time
 import keyboard
-import threading
 
-event = threading.Event()
 while True:
     x = random.randint(600, 700)
     y = random.randint(200, 600)
     print(x)
     time.sleep(0.5)
     pag.moveTo(x, y, 0.5)
+
+    if keyboard.is_pressed('e'):
+        break
